6fdd8feed24f1a71c912b909c7831a58a728b88c
Muhammad Shafiqul Islam <msqi@Muhammads-MacBook-Pro.local>
2025-07-18 17:03:27 +0600

    Redesigned the about

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -3,7 +3,7 @@
    ========================================================================== */
 
 $(document).ready(function(){
-   // Sticky footer
+  // Sticky footer
   var bumpIt = function() {
       $("body").css("margin-bottom", $(".page__footer").outerHeight(true));
     },
@@ -28,17 +28,11 @@ $(document).ready(function(){
 
   var stickySideBar = function(){
     var show = $(".author__urls-wrapper button").length === 0 ? $(window).width() > 1024 : !$(".author__urls-wrapper button").is(":visible");
-    // console.log("has button: " + $(".author__urls-wrapper button").length === 0);
-    // console.log("Window Width: " + windowWidth);
-    // console.log("show: " + show);
-    //old code was if($(window).width() > 1024)
     if (show) {
-      // fix
       Stickyfill.rebuild();
       Stickyfill.init();
       $(".author__urls").show();
     } else {
-      // unfix
       Stickyfill.stop();
       $(".author__urls").hide();
     }
@@ -51,7 +45,6 @@ $(document).ready(function(){
   });
 
   // Follow menu drop down
-
   $(".author__urls-wrapper button").on("click", function() {
     $(".author__urls").fadeToggle("fast", function() {});
     $(".author__urls-wrapper button").toggleClass("open");
@@ -65,34 +58,69 @@ $(document).ready(function(){
 
   // Magnific-Popup options
   $(".image-popup").magnificPopup({
-    // disableOn: function() {
-    //   if( $(window).width() < 500 ) {
-    //     return false;
-    //   }
-    //   return true;
-    // },
     type: 'image',
     tLoading: 'Loading image #%curr%...',
     gallery: {
       enabled: true,
       navigateByImgClick: true,
-      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+      preload: [0,1]
     },
     image: {
       tError: '<a href="%url%">Image #%curr%</a> could not be loaded.',
     },
-    removalDelay: 500, // Delay in milliseconds before popup is removed
-    // Class that is added to body when popup is open.
-    // make it unique to apply your CSS animations just to this exact popup
+    removalDelay: 500,
     mainClass: 'mfp-zoom-in',
     callbacks: {
       beforeOpen: function() {
-        // just a hack that adds mfp-anim class to markup
         this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
       }
     },
     closeOnContentClick: true,
-    midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+    midClick: true
   });
 
+  // =======================================================================
+  // NEW: Typing Animation Script
+  // =======================================================================
+  const textSpan = document.getElementById("identity-text");
+  const cursorSpan = document.querySelector(".typing-container .cursor");
+
+  // Check if the elements exist on the page before running the script
+  if (textSpan && cursorSpan) {
+    const identities = ["a Fulbrighter.", "an Academic.", "a Researcher.", "a Data Scientist."];
+    const typingDelay = 100;
+    const erasingDelay = 50;
+    const newTextDelay = 2000;
+    let textArrayIndex = 0;
+    let charIndex = 0;
+
+    function type() {
+      cursorSpan.classList.add('typing');
+      if (charIndex < identities[textArrayIndex].length) {
+        textSpan.textContent += identities[textArrayIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, typingDelay);
+      } else {
+        cursorSpan.classList.remove('typing');
+        setTimeout(erase, newTextDelay);
+      }
+    }
+
+    function erase() {
+      cursorSpan.classList.add('typing');
+      if (charIndex > 0) {
+        textSpan.textContent = identities[textArrayIndex].substring(0, charIndex - 1);
+        charIndex--;
+        setTimeout(erase, erasingDelay);
+      } else {
+        cursorSpan.classList.remove('typing');
+        textArrayIndex = (textArrayIndex + 1) % identities.length;
+        setTimeout(type, typingDelay + 1100);
+      }
+    }
+
+    if (identities.length) {
+      setTimeout(type, newTextDelay + 250);
+    }
+  }
 });
